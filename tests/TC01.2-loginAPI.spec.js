6e8661202e27310b2be08a9cfb5b0edf5800bdcd
ravigitgit <ravitiwari31415@gmail.com>
2026-05-03 13:09:26 +0530

    fixed API login test

--- tests/TC01.2-loginAPI.spec.js
+++ tests/TC01.2-loginAPI.spec.js
@@ -2,20 +2,28 @@ import { test, expect } from '../fixtures/auth.fixture';
 
 test.describe('API Auth Tests', () => {
 
-  test('Get user with saved session', async ({ request, authToken, userId }) => {
+  test('Get user via API session', async ({ request }) => {
+
+    const loginRes = await request.post('/Account/v1/Login', {
+      data: {
+        userName: process.env.DEMOQA_USERNAME,
+        password: process.env.DEMOQA_PASSWORD
+      }
+    });
+
+    const loginBody = await loginRes.json();
+    const token = loginBody.token;
+    const userId = loginBody.userId;
 
     const response = await request.get(
       `/Account/v1/User/${userId}`,
       {
         headers: {
-          Authorization: `Bearer ${authToken}`
+          Authorization: `Bearer ${token}`
         }
       }
     );
 
-    const body = await response.json();
-
-    console.log(body);
     expect(response.status()).toBe(200);
   });
 });
